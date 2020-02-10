fd85c90a907736918e0c3e7b3a2475bb3cd3f949
Yann <yann.moulaire@orange.fr>
2020-02-10 16:17:13 +0100

    POJO creation
    - User
    - Group
    - Payment

--- app/src/main/java/com/vision/telecount/com/vision/telecount/entity/User.java
+++ app/src/main/java/com/vision/telecount/com/vision/telecount/entity/User.java
@@ -1,4 +1,88 @@
 package com.vision.telecount.com.vision.telecount.entity;
 
+import java.util.List;
+import java.util.Objects;
+
 public class User {
+    private String email;
+    private String password;
+    private String lastName;
+    private String firstName;
+    private List<Group> groups;
+
+    public User(String email, String password, String lastName, String firstName, List<Group> groups) {
+        this.email = email;
+        this.password = password;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.groups = groups;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public List<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(groups, user.groups);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, password, lastName, firstName, groups);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", groups=" + groups +
+                '}';
+    }
 }
