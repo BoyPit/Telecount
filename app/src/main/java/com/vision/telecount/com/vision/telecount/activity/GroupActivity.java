883b9effa6a4587c313c2dde02c417f1c6cfde0a
BoyPit <innkdev@gmail.com>
2020-02-11 17:11:10 +0100

    Caca yann

--- app/src/main/java/com/vision/telecount/com/vision/telecount/activity/GroupActivity.java
+++ app/src/main/java/com/vision/telecount/com/vision/telecount/activity/GroupActivity.java
@@ -12,5 +12,7 @@ public class GroupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+        setTitle("Hello StackOverflow");
+
     }
 }
