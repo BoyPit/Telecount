8172ff8c8d34ca0c6a63148f67e5bda2c317e57d
BoyPit <innkdev@gmail.com>
2020-02-11 15:56:00 +0100

    Caca yann

---
+++ app/src/main/java/com/vision/telecount/com/vision/telecount/activity/GroupActivity.java
@@ -0,0 +1,16 @@
+package com.vision.telecount.com.vision.telecount.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vision.telecount.R;
+
+public class GroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
