584a851005a93f6bcbe0cdf8ce29372391eb32aa
BoyPit <innkdev@gmail.com>
2020-02-12 15:41:46 +0100

    Merge Theo

--- app/src/main/java/com/vision/telecount/com/vision/telecount/activity/DisplayGroupActivity.java
+++ app/src/main/java/com/vision/telecount/com/vision/telecount/activity/DisplayGroupActivity.java
@@ -2,9 +2,14 @@ package com.vision.telecount.com.vision.telecount.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.vision.telecount.R;
+
+import java.io.Serializable;
 
 public class DisplayGroupActivity extends AppCompatActivity {
 
@@ -13,6 +18,19 @@ public class DisplayGroupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.displaygroup);
         setTitle("Titre de l'Evenement");
+
+
+
+
+        FloatingActionButton bt =    (FloatingActionButton) findViewById(R.id.floating_action_button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               // startActivity(new );
+
+            }
+        });
 
     }
 }
